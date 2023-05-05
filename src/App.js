791668a5f4874956bf08f0698b168e4cd665707f
CristalFernandez <116036862+CristalFernandez@users.noreply.github.com>
2023-05-05 09:31:04 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Login } from "./Login";
+import { Registro } from "./Registro";
+import { Recuperacion} from "./Recuperacion";
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
